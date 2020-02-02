5b75257707d62c0ba3648380b79c3f7f3e50de44
Quang Huy Trần <traquahu1997@gmail.com>
2020-02-02 21:12:56 +0700

    add swagger + api product

---
+++ TeduShop.Web/Api/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TeduShop.Service;
+using TeduShop.Web.Infrastructure.Core;
+
+namespace TeduShop.Web.Api
+{
+    public class ProductController : ApiControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IErrorService errorService, IProductService productService) : base(errorService)
+        {
+            this._productService = productService;
+        }
+
+
+    }
+}
