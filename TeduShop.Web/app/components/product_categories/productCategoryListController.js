86fdeb61245af205f518f2ca6990204f8c0607fe
Quang Huy Trần <traquahu1997@gmail.com>
2020-02-03 22:53:33 +0700

    Delete Multi

--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,20 +1,49 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService', '$ngBootbox'];
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService', '$ngBootbox', '$filter'];
 
-    function productCategoryListController($scope, apiService, notificationService, $ngBootbox) {
+    function productCategoryListController($scope, apiService, notificationService, $ngBootbox, $filter) {
         $scope.productCategory = [];
 
         $scope.page = 0;
         $scope.pagesCount = 0;
 
         $scope.getListProductCategories = getProductCategories;
-        
+
         $scope.keyword = '';
 
         $scope.search = search;
         $scope.deleteProductCategory = deleteProductCategory;
+        $scope.deleteMulti = deleteMulti;
+
+        $scope.selectAll = selectAll;
+        $scope.isAll = false;
+
+        function selectAll() {
+            if ($scope.isAll === false) {
+                angular.forEach($scope.productCategory, function (item) {
+                    item.checked = true
+                        ;
+                });
+                $scope.isAll = true;
+            } else {
+                angular.forEach($scope.productCategory, function (item) {
+                    item.checked = false;
+                });
+                $scope.isAll = false;
+            }
+        }
+
+        $scope.$watch("productCategory", function (n, o) {
+            var checked = $filter("filter")(n, { checked: true });
+            if (checked.length) {
+                $scope.selected = checked;
+                $('#btnDelete').removeAttr('disabled');
+            } else {
+                $('#btnDelete').attr('disabled', 'disabled');
+            }
+        }, true);
 
         function deleteProductCategory(id) {
             $ngBootbox.confirm('Bạn có đồng ý xóa???').then(function () {
@@ -32,6 +61,24 @@
             });
         }
 
+        function deleteMulti() {
+            var listId = [];
+            $.each($scope.selected, function (i, item) {
+                listId.push(item.ID);
+            });
+            var config = {
+                params: {
+                    checkedProductCategories: JSON.stringify(listId)
+                }
+            };
+            apiService.del('api/ProductCategory/DeleteMulti', config, function (result) {
+                notificationService.displaySuccess('Xóa thành công ' + result.data + ' bản ghi.');
+                search();
+            }, function (error) {
+                notificationService.displayError('Xóa không thành công');
+            });
+        }
+
         function search() {
             getProductCategories();
         }
@@ -46,9 +93,9 @@
                     page: page,
                     pageSize: 2
                 }
-            }
+            };
 
-            apiService.get('/api/ProductCategory/GetAll', config, function(result) {
+            apiService.get('/api/ProductCategory/GetAll', config, function (result) {
                 if (result.data.TotalCount == 0) {
                     notificationService.displayWarning('Không tìm thấy bản ghi nào');
                 }
@@ -57,7 +104,7 @@
                 $scope.pagesCount = result.data.TotalPages;
                 $scope.totalCount = result.data.TotalCount;
             }, function () {
-                    console.log('Load failed');
+                console.log('Load failed');
             });
         }
 
